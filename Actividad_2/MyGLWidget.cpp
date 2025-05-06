50e27fa21dd1da3e3e97fc9e4af3dd33aeecab04
lariCFL <larissafpires@gmail.com>
2025-05-06 22:47:42 +0200

    test change position

--- Actividad_2/MyGLWidget.cpp
+++ Actividad_2/MyGLWidget.cpp
@@ -12,12 +12,12 @@ MyGLWidget::~MyGLWidget() {
 void MyGLWidget::initializeGL () {
     // Chama a inicialização de BL2GLWidget
     BL2GLWidget::initializeGL();
-    DEBUG("InitializeGL");
+    posRick = glm::vec3(0, 0, 0);
 }
 
 void MyGLWidget::paintGL() {
     // Chama a implementação de BL2GLWidget
     BL2GLWidget::paintGL();
-    DEBUG("PaintGL");
+
 }
 
