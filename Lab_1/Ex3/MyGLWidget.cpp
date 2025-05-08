a9b8f06ba6cb3b6adc9fbc62edfbfe93cb64a71b
lariCFL <larissafpires@gmail.com>
2025-05-08 20:29:32 +0200

    ex2 finish

--- Lab_1/Ex3/MyGLWidget.cpp
+++ Lab_1/Ex3/MyGLWidget.cpp
@@ -30,12 +30,7 @@ void MyGLWidget::paintGL ( ){
 }
 
 void MyGLWidget::projectTransform() {
-
-    float FOV = float(M_PI) / 2.0f; // Field of View
-    float ra = 1.0f; // Aspect Ratio
-    float znear = 0.4f; // Near plane
-    float zfar = 3.0f; // Far plane
-
+    ini_camera();
     viewTransform();
    
     // Set the projection matrix
@@ -46,15 +41,24 @@ void MyGLWidget::projectTransform() {
 }
 
 void MyGLWidget::viewTransform() {
-    glm::vec3 OBS = glm::vec3(0, 0, 1); // Camera position
-    glm::vec3 VRP = glm::vec3(0, 0, 0); // Look at point
-    glm::vec3 UP = glm::vec3(0, 1, 0); // Up vector
-    
     // Set the view matrix
     glm::mat4 View = glm::lookAt(OBS, VRP, UP);
     
     // Set the view matrix in the shader
     glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &View[0][0]);
     DEBUG("ViewTransform");
+}
+
+void MyGLWidget::ini_camera() {
+    // Initialize camera parameters
+    OBS = glm::vec3(0, 0, 1);
+    VRP = glm::vec3(0, 0, 0);
+    UP = glm::vec3(0, 1, 0);
+    FOV = float(M_PI) / 2.0f;
+    ra = 1.0f;
+    znear = 0.4f;
+    zfar = 3.0f;
+
+    DEBUG("ini_camera");
 }
 
